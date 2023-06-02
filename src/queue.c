e35c6e2a1cb7a844013a52d2337b7565dca3ab55
Nicolas Rossi <nrossi@itba.edu.ar>
2023-06-02 00:23:33 -0300

    Added queue implementation

---
+++ src/queue.c
@@ -0,0 +1,158 @@
+#include <queue.h>
+#include <string.h>
+#include <emalloc.h>
+
+static node * removeElementRec(queue *q, node *prev, node *current, void *element,
+                               bool (*compareElements)(const void *, const void *));
+
+queue * newQueue (size_t elementSize) {
+    queue * q = (queue *) malloc(sizeof(queue));
+    if (q == NULL) {
+        return NULL;
+    }
+    q->elementSize = elementSize;
+    q->size = 0;
+    q->head = q->tail = NULL;
+
+    return q;
+}
+
+queue * add (queue * q, void * element) {
+    if (q == NULL) {
+        return NULL;
+    }
+
+    node * n = (node *) emalloc(sizeof(node));
+    n->element = emalloc(q->elementSize);
+    memcpy(n->element, element, q->elementSize);
+    n->next = NULL;
+
+    if (q->head == NULL) {
+        q->head = q->tail = n;
+    } else {
+        q->tail->next = n;
+        q->tail = n;
+    }
+    q->size++;
+
+    return q;
+}
+
+void * poll (queue * q) {
+    if (isEmpty(q)) {
+        return NULL;
+    }
+
+    node * n = q->head;
+    q->head = q->head->next;
+    q->size--;
+    void * element = emalloc(q->elementSize);
+    memcpy(element, n->element, q->elementSize);
+    free(n->element);
+    free(n);
+
+    return element;
+}
+
+void * peek (queue * q) {
+    if (isEmpty(q)) {
+        return NULL;
+    }
+
+    return q->head->element;
+}
+
+void * peekTail (queue * q) {
+    if (isEmpty(q)) {
+        return NULL;
+    }
+
+    return q->tail->element;
+}
+
+queue * removeElement(queue *q, void *element, bool (*compareElements)(const void *, const void *)) {
+    q->head = removeElementRec(q, NULL, q->head, element, compareElements);
+
+    if (q->head == NULL) {
+        q->tail = NULL;
+    }
+
+    q->size--;
+    return q;
+}
+
+static node * removeElementRec(queue *q, node *prev, node *current, void *element,
+                               bool (*compareElements)(const void *, const void *)) {
+    if (current == NULL) {
+        return NULL;
+    }
+    node *aux;
+    if (compareElements(current->element, element)) {
+        aux = prev;
+        node *next = current->next;
+        if (current == q->tail) {
+            q->tail = aux;
+        }
+        if (current == q->head) {
+            q->head = current->next;
+        }
+        free(current);
+        return next;
+    }
+    aux = current;
+    current->next = removeElementRec(q, aux, current->next, element, compareElements);
+    return current;
+}
+
+bool isEmpty(queue * q) {
+    return size(q) == 0;
+}
+
+size_t size (queue * q) {
+    return q->size;
+}
+
+queue * clearQueue(queue * q) {
+    if (q == NULL) {
+        return NULL;
+    }
+
+    while (!isEmpty(q)) {
+        poll(q);
+    }
+
+    return q;
+}
+
+void destroyQueue(queue ** q) {
+    if (q == NULL || *q == NULL) {
+        return;
+    }
+
+    clearQueue(*q);
+    free(*q);
+    *q = NULL;
+}
+
+queueIterator * newQueueIterator(const queue *q) {
+    queueIterator *iter = (queueIterator *) emalloc(sizeof(queueIterator));
+    iter->queue = q;
+    iter->current = q->head;
+
+    return iter;
+}
+
+bool hasNext(const queueIterator *iter) {
+    return iter->current != NULL;
+}
+
+void* next(queueIterator *iter) {
+    if (!hasNext(iter)) {
+        return NULL; // No more elements
+    }
+
+    void *element = iter->current->element;
+    iter->current = iter->current->next;
+
+    return element;
+}
---
+++ test/queue_test.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
+#include <queue.h>
+
+START_TEST (integration_test_queue) {
+    queue * q = newQueue(sizeof(int));
+    ck_assert_uint_eq(0, size(q));
+    ck_assert_ptr_eq(NULL, peek(q));
+
+    q = add(q, &(int){1});
+    q = add(q, &(int){3});
+    q = add(q, &(int){5});
+    ck_assert_uint_eq(3, size(q));
+    ck_assert_uint_eq(1, *(int *)(peek(q)));
+    ck_assert_uint_eq(5, *(int *)(peekTail(q)));
+    ck_assert_uint_eq(3, size(q));
+    ck_assert_uint_eq(1, *(int *)poll(q));
+    ck_assert_uint_eq(5, *(int *)(peekTail(q)));
+    ck_assert_uint_eq(3, *(int *)poll(q));
+
+    q = clearQueue(q);
+    ck_assert_uint_eq(0, size(q));
+    destroyQueue(&q);
+} END_TEST
+
+START_TEST (test_empty_queue) {
+    queue * q = newQueue(sizeof(void *));
+
+    bool empty = isEmpty(q);
+    ck_assert_int_eq(true, empty);
+
+    ck_assert_ptr_eq(NULL, peek(q));
+    ck_assert_ptr_eq(NULL, poll(q));
+
+    destroyQueue(&q);
+}
+
+bool compareElements(const void * a, const void * b) {
+    return *(int *)a == *(int *)b;
+}
+
+START_TEST (test_iterate_remove_queue) {
+    queue * q = newQueue(sizeof(int));
+
+    int one   = 1;
+    int two   = 2;
+    int three = 3;
+    int four  = 4;
+    int five  = 5;
+    q = add(q, &one);
+    q = add(q, &two);
+    q = add(q, &three);
+    q = add(q, &four);
+    q = add(q, &five);
+
+    printf("Before removing elements...\n");
+    queueIterator * it = newQueueIterator(q);
+    while (hasNext(it)) {
+        int element = *(int *)next(it);
+        printf("%d\n", element);
+    }
+
+    q = removeElement(q, &one,  compareElements);
+    q = removeElement(q, &four, compareElements);
+    q = removeElement(q, &five, compareElements);
+
+    printf("After removing elements...\n");
+    it = newQueueIterator(q);
+    while (hasNext(it)) {
+        int element = *(int *)next(it);
+        printf("%d\n", element);
+    }
+
+    ck_assert_uint_eq(2, *(int *)(peek(q)));
+    ck_assert_uint_eq(3, *(int *)(peekTail(q)));
+    ck_assert_uint_eq(2, size(q));
+
+} END_TEST
+
+Suite *
+suite(void) {
+    Suite *s;
+    TCase *tc;
+
+    s = suite_create("queue_utils");
+    tc = tcase_create("queue_utils");
+
+    tcase_add_test(tc, integration_test_queue);
+    tcase_add_test(tc, test_empty_queue);
+    tcase_add_test(tc, test_iterate_remove_queue);
+    suite_add_tcase(s, tc);
+
+    return s;
+}
+
+int
+main(void) {
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
