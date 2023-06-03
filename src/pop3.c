ff4480e82f610701dd5470eb0af2cf57878cd39c
Juan R. Castro <juancastro@itba.edu.ar>
2023-06-03 16:37:23 -0300

    Input output buffers, start of state machine

--- src/pop3.c
+++ src/pop3.c
@@ -1,6 +1,118 @@
 #include <stdlib.h>
+#include <sys/socket.h>
 #include "pop3.h"
-#include "netutils.h"
+
+// TODO: Handlers to parse and execute actions
+static const struct state_definition client_states[] = {
+    {
+        .state = POP3_GREETING_READ,
+        .on_arrival = NULL,
+        .on_departure = NULL,
+        .on_read_ready = NULL,
+        .on_write_ready = NULL,
+        .on_block_ready = NULL,
+    },
+    {
+        .state = POP3_GREETING_WRITE,
+        .on_arrival = NULL,
+        .on_departure = NULL,
+        .on_read_ready = NULL,
+        .on_write_ready = NULL,
+        .on_block_ready = NULL,
+    },
+    {
+        .state = POP3_AUTH_READ,
+        .on_arrival = NULL,
+        .on_departure = NULL,
+        .on_read_ready = NULL,
+        .on_write_ready = NULL,
+        .on_block_ready = NULL,
+    },
+    {
+        .state = POP3_AUTH_WRITE,
+        .on_arrival = NULL,
+        .on_departure = NULL,
+        .on_read_ready = NULL,
+        .on_write_ready = NULL,
+        .on_block_ready = NULL,
+    },
+    {
+        .state = POP3_TRANSACTION_READ,
+        .on_arrival = NULL,
+        .on_departure = NULL,
+        .on_read_ready = NULL,
+        .on_write_ready = NULL,
+        .on_block_ready = NULL,
+    },
+    {
+        .state = POP3_TRANSACTION_WRITE,
+        .on_arrival = NULL,
+        .on_departure = NULL,
+        .on_read_ready = NULL,
+        .on_write_ready = NULL,
+        .on_block_ready = NULL,
+    },
+    {
+        .state = POP3_UPDATE,
+        .on_arrival = NULL,
+        .on_departure = NULL,
+        .on_read_ready = NULL,
+        .on_write_ready = NULL,
+        .on_block_ready = NULL,
+    },
+    {
+        .state = POP3_CLOSE,
+        .on_arrival = NULL,
+        .on_departure = NULL,
+        .on_read_ready = NULL,
+        .on_write_ready = NULL,
+        .on_block_ready = NULL,
+    }};
+
+static void closeConnection(struct selector_key *key) {
+    struct client_data *data = key->data;
+
+    // TODO: Log disconnection
+
+    selector_unregister_fd(key->s, key->fd);
+    close(key->fd);
+
+    free(data);
+}
+
+/* Handlers for the selector events, so we can wrap the state machine calls */
+static void pop3Read(struct selector_key *key);
+static void pop3Write(struct selector_key *key);
+static void pop3Block(struct selector_key *key);
+static void pop3Close(struct selector_key *key);
+static fd_handler pop3Handlers = {
+    .handle_read = pop3Read,
+    .handle_write = pop3Write,
+    .handle_block = pop3Block,
+    .handle_close = pop3Close,
+};
+
+void pop3Read(struct selector_key *key) {
+    struct state_machine *stm = &((struct client_data *) key->data)->stm;
+    stm_handler_read(stm, key);
+}
+
+void pop3Write(struct selector_key *key) {
+    struct state_machine *stm = &((struct client_data *) key->data)->stm;
+    stm_handler_write(stm, key);
+}
+
+void pop3Block(struct selector_key *key) {
+    struct state_machine *stm = &((struct client_data *) key->data)->stm;
+    stm_handler_block(stm, key);
+}
+
+void pop3Close(struct selector_key *key) {
+    struct state_machine *stm = &((struct client_data *) key->data)->stm;
+    stm_handler_close(stm, key);
+    closeConnection(key);
+}
+
 
 void passiveAccept(struct selector_key *key) {
     struct sockaddr_storage clientAddr;
@@ -31,13 +143,22 @@ void passiveAccept(struct selector_key *key) {
         return;
     }
 
-    // I don't know if these will be useful
+    // I don't know if this will be useful
     data->addr = clientAddr;
     data->addrLen = clientAddrLen;
 
+    buffer_init(&data->inputBuffer, BUFFER_SIZE, data->inputBufferData);
+    buffer_init(&data->outputBuffer, BUFFER_SIZE, data->outputBufferData);
+
+    data->stm.initial = POP3_GREETING_READ;
+    data->stm.max_state = POP3_CLOSE;
+    data->stm.states = client_states;
+
+    stm_init(&data->stm);
+
     // TODO: Client handlers
 
-    selector_status selectorStatus = selector_register(key->s, clientSocket, NULL, OP_READ | OP_WRITE, data);
+    selector_status selectorStatus = selector_register(key->s, clientSocket, &pop3Handlers, OP_READ | OP_WRITE, data);
 
     if (selectorStatus != SELECTOR_SUCCESS) {
         // TODO: Log error
