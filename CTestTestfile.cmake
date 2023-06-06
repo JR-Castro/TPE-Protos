5f293d0aab47a5711a5a7d8d342ad3514239631e
Nicolas Rossi <nrossi@itba.edu.ar>
2023-06-05 21:52:21 -0300

    .gitignore --- exclude cmake files

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nrossi/workspace/protos/TPE-Protos
-# Build directory: /home/nrossi/workspace/protos/TPE-Protos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
