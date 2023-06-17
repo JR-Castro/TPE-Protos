05f3cc33db81eda8374a9ccd2c3a417cc7de8c91
Juan R. Castro <juancastro@itba.edu.ar>
2023-06-17 16:25:19 -0300

    Add metrics

---
+++ src/include/metrics.h
@@ -0,0 +1,23 @@
+
+#ifndef TPE_PROTOS_METRICS_H
+#define TPE_PROTOS_METRICS_H
+
+#include <stddef.h>
+
+struct metrics {
+    size_t currentConnections;
+    size_t totalConnections;
+    size_t bytesSent;
+};
+
+void metrics_init();
+
+void metricsNewConnection();
+
+void metricsConnectionClosed();
+
+void metricsBytesSent(size_t bytes);
+
+void getMetrics(struct metrics *copy);
+
+#endif //TPE_PROTOS_METRICS_H
