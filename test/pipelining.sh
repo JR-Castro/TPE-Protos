910c118ed42ebf11e157e4313fced186545db718
magflores <magflores@itba.edu.ar>
2023-06-23 10:11:38 -0300

    Fix pipelining test
    Co-authored-by: Camila Sierra <camisie@users.noreply.github.com>

--- test/pipelining.sh
+++ test/pipelining.sh
@@ -3,4 +3,4 @@
 cd /home/magui/Desktop/TPE-Protos/cmake-build-debug/src
 ./pop3server -d /home/magui/Desktop/TPE-Protos/.mails -u magflores:password
 
-printf "USER magflores\nPASS password\nCAPA\nLIST\n" | nc -C -v localhost 1110
+printf "USER magflores\nPASS password\nCAPA\nLIST\n" | nc -C -v 127.0.0.1 1110
